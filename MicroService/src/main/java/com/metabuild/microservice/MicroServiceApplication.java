fd9129e5abac4fb58308b434b2e53086f0181495
seokchang <seokchang_h@naver.com>
2021-06-03 17:33:13 +0900

    Added Eureka Server & Eureka Client & netflix ribbon

--- MicroService/src/main/java/com/metabuild/microservice/MicroServiceApplication.java
+++ MicroService/src/main/java/com/metabuild/microservice/MicroServiceApplication.java
@@ -2,12 +2,14 @@ package com.metabuild.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServiceApplication.class, args);
 	}
-
+	
 }
